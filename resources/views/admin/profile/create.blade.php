da481724d28c73d560a6741cbd462ab61879c7c9
Aoi <blue89azul@gmail.com>
2020-07-11 08:12:29 +0000

    11.07.2020

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,58 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>MYプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    @if(count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="your_name" value="{{ old('your_name')}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <!--まずグリッドdivタグでレイアウト決めてから、項目の調整-->
+                        <div class="col-md-10"> 
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio"  name="male" value="0">男性
+                            <input class="form-check-input"  type="radio"  name="female" value="1">女性                        
+                        </div>  
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                        <select class="form-control">
+                            <option name="noSelect">選択して下さい</option>
+                            <option name="sport" value="1">スポーツ</option>
+                            <option name="cook" value="2">料理</option>
+                            <option name="shopping" value="3">買い物</option>
+                            <option name="readBook" value="4">読書</option>
+                            <option name="other" value="5">その他</option>
+                        </select>                           
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="selfIntro" value="{{ old('selfIntro') }}" rows="20" ></textarea>
+                        </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-secondary" name="更新">
+                </form>
             </div>
         </div>
     </div>
